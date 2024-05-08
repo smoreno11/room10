44c3921f3d2c2d309335e82e755bec6db3005571
Subject0023 <subject0023@thefoundation>
2024-05-08 01:17:27 +0000

    Added Room 23

--- game.c
+++ game.c
@@ -1,5 +1,5 @@
 //contributors
-//
+// Subject 0023
 
 #include <stdlib.h>
 #include <ctype.h>
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+
+void processRoom23();
 
 int main(int argc, char *argv[])
 {
@@ -144,6 +146,7 @@ int main(int argc, char *argv[])
 			case 23:
 			{
 				puts("room23");
+				processRoom23();
 				break;
 			}
 			case 24:
@@ -321,51 +324,65 @@ int main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
+void processRoom23() 
+{
+	// Initialize temporary variables
+	int choice = 99, randItemIndex;
+	bool loopVal = true;
+	char items[5][10] = {"Sword", "Shield", "Bow", "Staff", "Dagger"};
 
+	// Initialize random number generator by time
+	srand(time(NULL));
 
+	// Outputting the room number
+	printf("You have entered room 23.\n\n");
 
+	// Loop until loopVal is false
+	while(loopVal)
+	{
+		// Prompt user to enter an option
+		printf("Please select an option:\n"
+			"(1) Examine the room.\n"
+			"(2) Rummage through your belongings.\n"
+			"(3) Search the room for entities.\n"
+			"(4) Scavenge the room for items.\n"
+			"(0) Exit the room.\n\n");
 
+		// Receive user input
+		printf("Selecting: ");
+		scanf("%d", &choice);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		// Switch case based on choices from 0 - 4, inclusive
+		switch(choice)
+		{
+			// Exit the room
+			case 0:
+				printf("Exiting the room...\n");
+				loopVal = false;
+				break;
+			// Examine the room
+			case 1:
+				printf("The room appears to be an empty, white box with a ceiling lamp.\n\n");
+				break;
+			// Rummage through your belongings
+			case 2:
+				printf("You search through your belongings, taking out an energy potion to refill your stamina.\n\n");
+				break;
+			// Search the room for entities
+			case 3:
+				printf("A shadowy, beast figure crawls about on the ceiling.\n\n");
+				break;
+			// Scavenge the room for items
+			case 4:
+				printf("Searching for items in the room...\n");
+				randItemIndex = rand() % 5;
+				printf("You have found a(n): %s!\n\n", items[randItemIndex]);
+				break;
+			// Default case, output an error on incorrect input
+			default:
+				printf("Error. Something went wrong!\n");
+				loopVal = false;
+				break;
+		}
+	}
+}
