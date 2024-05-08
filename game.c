ce5c12af0b8f7c4423787a3e940a67aff3762f08
Suave714 <bguzman12@email.com>
2024-05-08 01:25:08 +0000

    room 32 added

--- game.c
+++ game.c
@@ -1,5 +1,5 @@
 //contributors
-//
+//Suave714
 
 #include <stdlib.h>
 #include <ctype.h>
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+
+void generateGold();
 
 int main(int argc, char *argv[])
 {
@@ -189,8 +191,118 @@ int main(int argc, char *argv[])
 			}
 			case 32:
 			{
-				puts("room32");
-				break;
+				int pathNum = 0;
+				int choice = 0;
+				srand(time(NULL));
+				int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+	puts("Welcome! You have entered room 32!\n");
+	puts("There are 5 Paths around the room.\n");
+
+	for(int i = 0; i < 5; i++){
+		puts("Path options.");
+		puts("Path 1 says 'Take a chance!'.");
+                puts("Path 2 says 'Be aware DO NOT ENTER'.");
+                puts("Path 3 says 'Enter at your own RISK'.");
+                puts("Path 4 says 'Goodies'.");
+                puts("Path 5 is across the main door you entered from. It is a long dark pathway seeming to be endless.\n");
+		printf("Please choose a path number from 1 - 5: \n");
+		scanf("%d", &pathNum);
+
+		if(pathNum < 1 || pathNum > 5)
+		{
+			puts("Invalid path!");
+			puts("Please chose a path between 1 - 5: ");
+			scanf("%d", &pathNum);
+		}
+		switch(pathNum)
+		{
+			case 1:
+				puts("You chose path 1.\n");
+				puts("You have encountered a Wizard at the end of the pathway.");
+				puts("The Wizard has 3 chests in front of him.\n");
+				puts("The Wizard says:");
+				puts("Who is there?");
+				puts("Ahhh its a traveler!");
+				puts("Welcome!");
+				puts("I have something for you.");
+			do{
+				puts("Choose a chest 1 - 3:");
+                                scanf("%d", &choice);
+				switch (choice) 
+				{
+					case 1:
+						puts("You open the chest and a puff of smoke hit you in the face!");
+						puts("You ask the wizard what was that?");
+						puts("He says you will see");
+						puts("you feel a bit weird.");
+						puts("The Wizard sent you back.\n");
+						break;
+					case 2:
+						generateGold();
+						puts("The Wizard sent you back.\n");
+						break;
+					case 3:
+						puts("You currently only have flipflops and you found some old shoes.");
+						puts("The Wizard tells you that these shoes are better for you.");
+						puts("He convinced you to put them on.");
+						puts("The Wizard sent you back.\n");
+						break;
+					default:
+						printf("What do you mean %d! Do you see me a as fool!\n", choice);
+						break;
+				}
+				} while(choice < 1 || choice > 3);
+			continue;
+		case 2: 
+			puts("You chose path 2.\n");
+			puts("You follow the path to only reach a dead end.");
+			puts("All of a sudden the ceiling started to collapse!");
+			puts("You managed to get away nearly escaping death.");
+			puts("You returned the way you came from.\n");
+			puts("Choose a new path.\n");
+
+			continue;
+		case 3: 
+			puts("You chose path 3.\n");
+			puts("You come across an opening to an under ground cave system.");
+			puts("You slowly approach since there is no light.");
+			puts("You slip on some damp rocks and almost fall into the cave system.");
+			puts("Out of fear of almost falling into the cave system you return.");
+			puts("Choose a new path.");
+			continue;
+		case 4: 
+			puts("You chose path 4.\n");
+			puts("You see a leprechaun sitting down.");
+			puts("As you get closer you see a pot of gold sitting infront of him.\n");
+			puts("He says:");
+			puts("Oh I have a treat for you!");
+			puts("Go ahead and get a hand full of coins out of my pot!");
+			generateGold();
+			puts("You are very welcome ungreatful human!");
+			puts("He send you on your way.");
+			puts("You returned.");
+			puts("Choose a new path.\n");
+			continue;
+		case 5: 
+			puts("You chose path 5.\n");
+			puts("You follow the path for what seems forever.");
+			puts("You encounter some bugs here and there for hours.");
+			puts("You finally see some light at the end of the path!");
+			puts("You came to realize that the wizard had put a trap spell on you.");
+			puts("You see a pool of water, you decide to wipe your face and drink some water.");
+			puts("The effects wore off!");
+			puts("It was all an illusion and a dead end appeared.");
+			puts("You returned");
+			puts("You are now leaving room 32!");
+			continue;
+		default:
+			puts("Invalid path! Please enter another path.\n");
+			break;
+	}
+	
+}				break;
 			}
 
 			case 33:
@@ -321,6 +433,16 @@ int main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
+void generateGold() {
+	int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int randomArrIndex = 0;
+	int randomNumber = 0;
+	
+	randomArrIndex = rand() % 10;
+	randomNumber = numbers[randomArrIndex];
+	
+	printf("You found %d gold coins!\n", randomNumber);
+}
 
 
 
